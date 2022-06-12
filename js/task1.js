a85a17a9eea9c235a6402fec78bf8bc49206d729
Roman13456 <romankhromishin@gmail.com>
2022-06-12 08:15:38 +0300

    add new capabilities

--- js/task1.js
+++ js/task1.js
@@ -12,21 +12,32 @@ let removeBtn = document.querySelector(".btn.btn-outline-secondary")
 //     newInput.setAttribute("placeholder","info")
 //     form0.append(newInput)
 // })
-form.addEventListener("click",function(event){
-    console.log(event)
-
+form.addEventListener("click",function(event){ 
+    let inputContainer = document.createElement("div")
+    inputContainer.classList.add("input-group","mb-3")
+    
     let newInput = document.createElement("input")
-    newInput.classList.add("form-control","col-12","mb-3")
+    newInput.classList.add("form-control","col-12")
     newInput.setAttribute("placeholder","info")
+    inputContainer.append(newInput)
 
-    let inputArr = document.querySelectorAll(".form-control")
-    let lastInput = inputArr[inputArr.length-1]
+    let removeBtnContainer = document.createElement("div") 
+    removeBtnContainer.classList.add("input-group-append")
+    inputContainer.append(removeBtnContainer)
+
+    let newRemoveBtn = document.createElement("button")
+    newRemoveBtn.classList.add("btn","btn-outline-secondary")
+    newRemoveBtn.setAttribute("type","button")
+    newRemoveBtn.innerHTML = "Remove"
+    removeBtnContainer.append(newRemoveBtn)
+    // let inputArr = document.querySelectorAll(".form-control")
+    // let lastInput = inputArr[inputArr.length-1]
     
     if(event.target.textContent ==="Add"){
-        form0.append(newInput)
+        form0.append(inputContainer)
     }
-    else if(event.target.textContent ==="Remove" && inputArr.length-1!==0){
-        lastInput.remove()
+    else if(event.target.textContent ==="Remove"){
+        event.target.parentElement.parentElement.remove()
     }
     
 })
